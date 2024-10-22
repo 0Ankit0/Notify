e30f15845d8ae47f51377ea27cb15af7ff7de23f
0ankit0 <ankitpaudel.1373@gmail.com>
2024-10-22 07:53:43 +0545

    Refactor DarkModeSwitch to use button with icons
    - Added `MoonIcon` component in `moon-icon.tsx` for moon SVG.
    - Added `SunIcon` component in `sun-icon.tsx` for sun SVG.
    - Removed `Switch` component from `@nextui-org/react` in `darkmodeswitch.tsx`.
    - Imported `MoonIcon` and `SunIcon` in `darkmodeswitch.tsx`.
    - Updated `DarkModeSwitch` to use a button for theme toggling.
    - Button displays `MoonIcon` for dark theme and `SunIcon` for light theme.

--- notify.client/src/components/navbar/darkmodeswitch.tsx
+++ notify.client/src/components/navbar/darkmodeswitch.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { useTheme as useNextTheme } from "next-themes";
-import { Switch } from "@nextui-org/react";
+import { MoonIcon } from "../icons/navbar/moon-icon";
+import { SunIcon } from "../icons/navbar/sun-icon";
 
 export const DarkModeSwitch = () => {
   const { setTheme, resolvedTheme } = useNextTheme();
+
   return (
-    <Switch
-      isSelected={resolvedTheme === "dark" ? true : false}
-      onValueChange={(e) => setTheme(e ? "dark" : "light")}
-    />
+    <button
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      className="p-2 focus:outline-none"
+      style={{ background: "none", border: "none" }}
+    >
+      {resolvedTheme === "dark" ? <MoonIcon /> : <SunIcon />}
+    </button>
   );
 };
