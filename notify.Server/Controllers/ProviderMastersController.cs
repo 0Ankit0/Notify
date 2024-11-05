8986c1c8a98b4219ad09282efd5eac65cddca61f
0ankit0 <ankitpaudel.1373@gmail.com>
2024-11-05 07:16:27 +0545

    Add new API controllers and update project dependencies
    Introduced new API controllers for managing entities:
    - MessagesController: CRUD for Message entities.
    - ProviderMastersController: CRUD for ProviderMaster entities.
    - UserMastersController: CRUD for UserMaster entities.
    - UserTokensController: CRUD for UserToken entities.
    
    Each controller supports:
    - Retrieving all entities (HttpGet).
    - Retrieving a single entity by ID (HttpGet("{id}")).
    - Updating an entity (HttpPut("{id}")).
    - Creating a new entity (HttpPost).
    - Deleting an entity (HttpDelete("{id}")).
    
    Updated notify.Server.csproj to include:
    - Microsoft.VisualStudio.Web.CodeGeneration.Design v8.0.6.

---
+++ notify.Server/Controllers/ProviderMastersController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Notify.Server.Data;
+using Notify.Server.Data.Providers;
+
+namespace notify.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProviderMastersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProviderMastersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProviderMasters
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProviderMaster>>> GetProviderMasters()
+        {
+            return await _context.ProviderMasters.ToListAsync();
+        }
+
+        // GET: api/ProviderMasters/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProviderMaster>> GetProviderMaster(int id)
+        {
+            var providerMaster = await _context.ProviderMasters.FindAsync(id);
+
+            if (providerMaster == null)
+            {
+                return NotFound();
+            }
+
+            return providerMaster;
+        }
+
+        // PUT: api/ProviderMasters/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProviderMaster(int id, ProviderMaster providerMaster)
+        {
+            if (id != providerMaster.ProviderId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(providerMaster).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProviderMasterExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProviderMasters
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProviderMaster>> PostProviderMaster(ProviderMaster providerMaster)
+        {
+            _context.ProviderMasters.Add(providerMaster);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProviderMaster", new { id = providerMaster.ProviderId }, providerMaster);
+        }
+
+        // DELETE: api/ProviderMasters/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProviderMaster(int id)
+        {
+            var providerMaster = await _context.ProviderMasters.FindAsync(id);
+            if (providerMaster == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProviderMasters.Remove(providerMaster);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProviderMasterExists(int id)
+        {
+            return _context.ProviderMasters.Any(e => e.ProviderId == id);
+        }
+    }
+}
