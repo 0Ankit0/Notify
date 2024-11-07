fa6d76e44bd97ce56fca45bfb8bc9869dce9757c
0ankit0 <ankitpaudel.1373@gmail.com>
2024-11-07 06:04:54 +0545

    Refactor UserTokensController to use CustomMethods
    Updated UserTokensController to utilize CustomMethods for mapping between UserToken and UserTokenModel. Modified constructors and method signatures to accommodate these changes. Adjusted HTTP attributes and updated using directives.

--- notify.Server/Controllers/UserTokensController.cs
+++ notify.Server/Controllers/UserTokensController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using notify.Server.Classes;
+using notify.Server.Models;
 using Notify.Server.Data;
 using Notify.Server.Data.Users;
 
@@ -15,22 +17,30 @@ namespace notify.Server.Controllers
     public class UserTokensController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private readonly CustomMethods _customMethods;
 
-        public UserTokensController(ApplicationDbContext context)
+        public UserTokensController(ApplicationDbContext context,CustomMethods customMethods)
         {
             _context = context;
+            _customMethods = customMethods;
         }
 
         // GET: api/UserTokens
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<UserToken>>> GetUserTokens()
+        public async Task<ActionResult<IEnumerable<UserTokenModel>>> GetUserTokens()
         {
-            return await _context.UserTokens.ToListAsync();
+            return await _context.UserTokens.Select(ut => new UserTokenModel
+            {
+                Id = ut.Id,
+                UserId = ut.UserId,
+                Token = ut.Token,
+                CreatedAt = ut.CreatedAt
+            }).ToListAsync();
         }
 
         // GET: api/UserTokens/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<UserToken>> GetUserToken(int id)
+        public async Task<ActionResult<UserTokenModel>> GetUserToken(int id)
         {
             var userToken = await _context.UserTokens.FindAsync(id);
 
@@ -38,19 +48,24 @@ namespace notify.Server.Controllers
             {
                 return NotFound();
             }
+            UserTokenModel userTokenModel = new UserTokenModel();
+            _customMethods.MapProperties(userToken, userTokenModel);
 
-            return userToken;
+            return userTokenModel;
         }
 
         // PUT: api/UserTokens/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUserToken(int id, UserToken userToken)
+        [HttpPut()]
+        public async Task<IActionResult> PutUserToken(UserTokenModel userTokenModel)
         {
-            if (id != userToken.Id)
+            int id = userTokenModel.Id ?? 0;
+            if (id ==0)
             {
                 return BadRequest();
             }
+            UserToken userToken = new UserToken();
+            _customMethods.MapProperties(userTokenModel, userToken);
 
             _context.Entry(userToken).State = EntityState.Modified;
 
@@ -76,12 +91,14 @@ namespace notify.Server.Controllers
         // POST: api/UserTokens
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<UserToken>> PostUserToken(UserToken userToken)
+        public async Task<ActionResult<UserTokenModel>> PostUserToken(UserTokenModel userTokenModel)
         {
+            UserToken userToken = new UserToken();
+            _customMethods.MapProperties(userTokenModel, userToken);
             _context.UserTokens.Add(userToken);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetUserToken", new { id = userToken.Id }, userToken);
+            return CreatedAtAction("GetUserToken", new { id = userToken.Id }, userTokenModel);
         }
 
         // DELETE: api/UserTokens/5
